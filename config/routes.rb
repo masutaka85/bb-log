91c9b9622b8078f1eac996513b0ec3adbe07dc53
masutaka85 <tkhr.bk67msd@gmail.com>
2020-11-05 00:16:09 +0900

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+devise_for :users, controllers: {
+  omniauth_callbacks: 'users/omniauth_callbacks',
+  registrations: 'users/registrations'
+}
   root to: "stats#index"
-  resources :stats, only: :index
+  resources :stats, only: [:index, :new]
 end
