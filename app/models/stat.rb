804ae35234e326bcd582927e4df7ba5f303365c5
masutaka85 <tkhr.bk67msd@gmail.com>
2020-11-05 17:38:30 +0900

    カラム、アソシエーションの追加

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,2 +1,11 @@
 class Stat < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :position
+  belongs_to :batting_order
+  belongs_to :kiyou
+
+  belongs_to :user
+  has_one :pitcher
+  has_one :fielder
+
 end
